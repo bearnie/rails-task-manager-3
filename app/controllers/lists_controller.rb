d7c2dca2d31f1ad1cbe34e54b555749c17f7560c
rgylling <mikegylling@yahoo.com>
2016-12-13 13:43:53 -0800

    fixing routing

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -35,7 +35,7 @@ class ListsController < ApplicationController
   def destroy
     @list = List.find(params[:id])
     @list.destroy
-    redirect_to 'lists'
+    redirect_to lists_path
   end
 
   private
