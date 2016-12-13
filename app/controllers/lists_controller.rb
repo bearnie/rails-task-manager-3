dc40713900a0e47aa8c347bc658eb6e647abcda0
rgylling <mikegylling@yahoo.com>
2016-12-13 11:04:11 -0800

    stubbing out my CRUD methods

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,3 +1,37 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user!
 
+  def index
+    @list = current_user.lists.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def list_params
+
+  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,37 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
 
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def task_params
+
+  end
 end
