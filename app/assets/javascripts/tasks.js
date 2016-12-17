edfe3190cd37efa9f6f2b5d6533a90384d4e5d2e
rgylling <mikegylling@yahoo.com>
2016-12-16 16:11:00 -0800

    new task officially works

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -4,3 +4,33 @@ function newTaskShow () {
     $(".create-task").show(1000);
   });
 }
+
+function completeButton(task) {
+  var complete = "";
+
+  if (task.complete) {
+    complete = "checked='checked'"
+  } else {
+    complete = ""
+  }
+
+  var completeButton = "<div class='float-right'><p class='task-list-text display-inline'><input type='checkbox' name='completed?'id='task_complete'value='1' disabled='disabled'" +  complete + "></p></div>";
+
+  return completeButton
+
+}
+
+function deleteButton (task, token) {
+
+  var myDeleteButton = "<form class='button_to' method='post' action='/lists/" + task.list.id + "/tasks/" + task.id + "'><input type='hidden' name='_method' value='delete'><input class='button-small btn' type='submit' value='Delete'><input type='hidden' name='authenticity_token' value=" + token + "></form>"
+
+  return myDeleteButton
+
+}
+
+function editButton (task) {
+
+  var myEditButton = "<form class='button_to margin-right-5' method='get' action='/lists/" + task.list.id  + "/tasks/" + task.id + "/edit'><input class='button-small btn light-green' type='submit' value='Edit'></form>";
+
+  return myEditButton
+}
