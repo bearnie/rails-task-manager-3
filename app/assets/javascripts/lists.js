68a45d4b153ea32dbc7ccf1b2295560026323905
rgylling <mikegylling@yahoo.com>
2016-12-17 13:23:42 -0800

    created task object

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,6 +1,7 @@
-function List(name, date){
+function List(name, date, id){
   this.name = name;
   this.due_date = date;
+  this.id = id
   this.date = function() {
     var monthNames = [
       "January", "February", "March",
@@ -23,37 +24,4 @@ function List(name, date){
 
     return finalString
   }
-
 }
-
-
-
-// function dateParser(date) {
-//   var monthNames = [
-//     "January", "February", "March",
-//     "April", "May", "June", "July",
-//     "August", "September", "October",
-//     "November", "December"
-//   ];
-//
-//   var date = new Date(date);
-//   var utcDate = date.toUTCString()
-//   var day = date.getUTCDate();
-//
-//   function formatDay (day) {
-//     var string = "0"
-//     if (day < 10) {
-//       currentDay = day.toString();
-//       string += currentDay
-//       return string
-//     } else {
-//       return day.toString();
-//     }
-//   }
-//
-//   var monthIndex = date.getUTCMonth();
-//   var year = date.getUTCFullYear();
-//   var finalString = (monthNames[monthIndex] + " " + formatDay(day) + ", " + year.toString());
-//
-//   return finalString
-// }
